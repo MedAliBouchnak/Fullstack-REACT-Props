40d059896f47f07ca6ac67a369e24ab6f97aeb33
MedAliBouchnak <106705873+MedAliBouchnak@users.noreply.github.com>
2022-10-30 16:59:30 +0100

    Final push, but have button Problem

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ function App() {
     "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur dolorum repellat minima natus! Dicta corporis ad fugit quidem ipsam a vitae aut accusantium provident nulla. Blanditiis eligendi neque sit quam.";
   const Profession = "Engineer";
   
-  const alertMe = (fullName) => alert({fullName});
+  
   return (
     <div className="App">
       <header className="App-header">
-        <Profile fullName={fullName} Bio={bio} profession={Profession} alertMe={alertMe}>
-          <img src={profileImage} alt="Profile" />
+        <Profile fullName={fullName} Bio={bio} profession={Profession}>
+          <img style={{width:600 ,borderRadius:500}} src={profileImage} alt="Profile" />
         </Profile>
       </header>
     </div>
--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -1,15 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 import profileDefault from "../Photo2.jpg";
-const Profile = ({ fullName, Bio, profession, children, alertMe }) => {
-  console.log(children);
+const Profile = ({
+  fullName,
+  Bio,
+  profession,
+  children,
+  alertMe = (fName) => alert(`${fName}`),
+}) => {
   return (
-    <div>
-      <h1>{fullName}</h1>
-      <h1>{profession}</h1>
-      <p>{Bio}</p>
-      <button onClick={alertMe({fullName})}>Click me</button>
+    <div style={{ margin:20, padding: "20 0 20 0", borderRadius: 10, display: "flex", gap: 20 }}>
       <div>{children}</div>
+      <div style={{ margin:20, padding: "20 0 20 0",  display: "flex", flexDirection: "column" }}>
+        <h1 style={{textAlign: "justify", fontFamily: "wotfardRegularWebfont", fontWeight: "600", fontSize: "3.4rem", marginBottom: 10 }}>{fullName}</h1>
+        <h1 style={{textAlign: "justify", lineHeight: "0.9", fontFamily: "wotfardRegularWebfont", fontWeight: "600", fontSize: "3.4rem" }}>{profession}</h1>
+        <p style={{ marginTop: 20, textAlign: "justify", lineHeight: "1.3", fontFamily: "wotfardRegularWebfont", fontSize: 20 }}>{Bio}</p>
+        <button style={{backgroundColor:'#e7e7e7', color: 'black',padding: "12px 28px"}} onClick={alertMe(fullName)} >Click me</button>
+      </div>s
+      
     </div>
   );
 };
@@ -23,6 +31,6 @@ Profile.PropsTypes = {
   fullName: PropTypes.string,
   Bio: PropTypes.string,
   profession: PropTypes.string,
-  children: PropTypes.string
+  children: PropTypes.string,
 };
 export default Profile;
