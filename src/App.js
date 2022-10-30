db4ea2f616f26c19d5c6f4f84b9ee54fa94fddc2
MedAliBouchnak <106705873+MedAliBouchnak@users.noreply.github.com>
2022-10-30 15:14:21 +0100

    Kmala

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import "./App.css";
 import Profile from "./Profile/Profile";
-import profileImage from "./Photo.jpg"
+import profileImage from "./Photo.jpg";
+
 function App() {
   const fullName = "Mohamed Ali Bouchnak";
   const bio =
     "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur dolorum repellat minima natus! Dicta corporis ad fugit quidem ipsam a vitae aut accusantium provident nulla. Blanditiis eligendi neque sit quam.";
   const Profession = "Engineer";
+  
+  const alertMe = (fullName) => alert({fullName});
   return (
     <div className="App">
       <header className="App-header">
-        <Profile fullName={fullName} Bio={bio} profession={Profession}>
+        <Profile fullName={fullName} Bio={bio} profession={Profession} alertMe={alertMe}>
           <img src={profileImage} alt="Profile" />
         </Profile>
       </header>
--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -1,16 +1,28 @@
-import React from 'react'
-
-const Profile = ({fullName,Bio,profession,children}) => {
-    console.log(children);
+import React from "react";
+import PropTypes from "prop-types";
+import profileDefault from "../Photo2.jpg";
+const Profile = ({ fullName, Bio, profession, children, alertMe }) => {
+  console.log(children);
   return (
-    
     <div>
-        <h1>{fullName}</h1>
-        <p>{Bio}</p>
-        <h1>{profession}</h1>
-        <img src={children} alt={fullName} />
+      <h1>{fullName}</h1>
+      <h1>{profession}</h1>
+      <p>{Bio}</p>
+      <button onClick={alertMe({fullName})}>Click me</button>
+      <div>{children}</div>
     </div>
-  )
-}
-
-export default Profile
+  );
+};
+Profile.defaultProps = {
+  fullName: "Dali Bouchnak",
+  Bio: "See linkdIn Profile",
+  profession: "GoMyCode Student",
+  children: <img src={profileDefault} alt="" />,
+};
+Profile.PropsTypes = {
+  fullName: PropTypes.string,
+  Bio: PropTypes.string,
+  profession: PropTypes.string,
+  children: PropTypes.string
+};
+export default Profile;
