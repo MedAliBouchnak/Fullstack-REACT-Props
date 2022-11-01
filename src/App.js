d6e33969ad6e4dd3959d465094b3fcc65b5c4e96
MedAliBouchnak <106705873+MedAliBouchnak@users.noreply.github.com>
2022-11-01 10:08:38 +0100

    Final Changes :fixing the button problem

--- src/App.js
+++ src/App.js
@@ -7,13 +7,24 @@ function App() {
   const bio =
     "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur dolorum repellat minima natus! Dicta corporis ad fugit quidem ipsam a vitae aut accusantium provident nulla. Blanditiis eligendi neque sit quam.";
   const Profession = "Engineer";
-  
-  
+
+  const alertMe = () => {
+    alert(`${fullName}`);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <Profile fullName={fullName} Bio={bio} profession={Profession}>
-          <img style={{width:600 ,borderRadius:500}} src={profileImage} alt="Profile" />
+        <Profile
+          fullName={fullName}
+          Bio={bio}
+          profession={Profession}
+          alertMe={alertMe}
+        >
+          <img
+            style={{ width: 600, borderRadius: 500 }}
+            src={profileImage}
+            alt="Profile"
+          />
         </Profile>
       </header>
     </div>
--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -1,23 +1,71 @@
 import React from "react";
 import PropTypes from "prop-types";
 import profileDefault from "../Photo2.jpg";
-const Profile = ({
-  fullName,
-  Bio,
-  profession,
-  children,
-  alertMe = (fName) => alert(`${fName}`),
-}) => {
+const Profile = ({ fullName, Bio, profession, children, alertMe }) => {
+  
   return (
-    <div style={{ margin:20, padding: "20 0 20 0", borderRadius: 10, display: "flex", gap: 20 }}>
+    <div
+      style={{
+        margin: 20,
+        padding: "20 0 20 0",
+        borderRadius: 10,
+        display: "flex",
+        gap: 20,
+      }}
+    >
       <div>{children}</div>
-      <div style={{ margin:20, padding: "20 0 20 0",  display: "flex", flexDirection: "column" }}>
-        <h1 style={{textAlign: "justify", fontFamily: "wotfardRegularWebfont", fontWeight: "600", fontSize: "3.4rem", marginBottom: 10 }}>{fullName}</h1>
-        <h1 style={{textAlign: "justify", lineHeight: "0.9", fontFamily: "wotfardRegularWebfont", fontWeight: "600", fontSize: "3.4rem" }}>{profession}</h1>
-        <p style={{ marginTop: 20, textAlign: "justify", lineHeight: "1.3", fontFamily: "wotfardRegularWebfont", fontSize: 20 }}>{Bio}</p>
-        <button style={{backgroundColor:'#e7e7e7', color: 'black',padding: "12px 28px"}} onClick={alertMe(fullName)} >Click me</button>
-      </div>s
-      
+      <div
+        style={{
+          margin: 20,
+          padding: "20 0 20 0",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <h1
+          style={{
+            textAlign: "justify",
+            fontFamily: "wotfardRegularWebfont",
+            fontWeight: "600",
+            fontSize: "3.4rem",
+            marginBottom: 10,
+          }}
+        >
+          {fullName}
+        </h1>
+        <h1
+          style={{
+            textAlign: "justify",
+            lineHeight: "0.9",
+            fontFamily: "wotfardRegularWebfont",
+            fontWeight: "600",
+            fontSize: "3.4rem",
+          }}
+        >
+          {profession}
+        </h1>
+        <p
+          style={{
+            marginTop: 20,
+            textAlign: "justify",
+            lineHeight: "1.3",
+            fontFamily: "wotfardRegularWebfont",
+            fontSize: 20,
+          }}
+        >
+          {Bio}
+        </p>
+        <button
+          style={{
+            backgroundColor: "#e7e7e7",
+            color: "black",
+            padding: "12px 28px",
+          }}
+          onClick={()=>alertMe()}
+        >
+          Click me
+        </button>
+      </div>
     </div>
   );
 };
